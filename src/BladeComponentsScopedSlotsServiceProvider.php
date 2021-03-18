5e2f198b76e949f81d0e6a830d3ca70831835fea
Steven <steven.demarco@while-not-false.ch>
2021-03-18 17:08:24 +0100

    add a precompiler tag

--- src/BladeComponentsScopedSlotsServiceProvider.php
+++ src/BladeComponentsScopedSlotsServiceProvider.php
@@ -2,13 +2,14 @@
 
 namespace KonradKalemba\BladeComponentsScopedSlots;
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class BladeComponentsScopedSlotsServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
+     * Bootstrap the scoped scope service provider
      *
      * @return void
      */
@@ -25,21 +26,47 @@ class BladeComponentsScopedSlotsServiceProvider extends ServiceProvider
             // Extract values from the directive's arguments array
             [$name, $functionArguments, $functionUses] = $directiveArguments;
 
-            // Connect the arguments to form a correct function declaration
-            if ($functionArguments) $functionArguments = "function {$functionArguments}";
-            
             $functionUses = array_filter(explode(',', trim($functionUses, '()')), 'strlen');
-            
-            // Add `$__env` to allow usage of other Blade directives inside the scoped slot
-            array_push($functionUses, '$__env');
 
+            // Add `$__env` and `$component` to allow usage of other Blade directives inside the scoped slot
+            $functionUses = array_merge($functionUses, ['$component', '$__env']);
             $functionUses = implode(',', $functionUses);
 
-            return "<?php \$__env->slot({$name}, {$functionArguments} use ({$functionUses}) { ?>"; 
+            return "<?php \$__env->slot({$name}, function({$functionArguments}) use ({$functionUses}) { ?>";
         });
 
         Blade::directive('endscopedslot', function () {
             return "<?php }); ?>";
         });
+
+        Blade::precompiler(function ($value) {
+            $value = preg_replace_callback('/<\s*x[\-\:]scoped-slot\s+(:?)name=(?<name>(\"[^\"]+\"|\\\'[^\\\']+\\\'|[^\s>]+))\s*(context=(?<context>(\"[^\"]+\"|\\\'[^\\\']+\\\'|[^\s>]+))\s*>/', function ($matches) {
+                $name = $this->stripQuotes($matches['name']);
+                $context = $this->stripQuotes($matches['context']);
+
+                if ($matches[1] !== ':') {
+                    $name = "'{$name}'";
+                }
+
+                return " @scopedslot({$name}, {$context}) ";
+            }, $value);
+
+            return preg_replace('/<\/\s*x[\-\:]scoped-slot[^>]*>/', ' @endscopedslot', $value);
+        });
+
     }
+
+    /**
+     * Strip any quotes from the given string. (Copied from Illuminate\View\Compilers\ComponentTagCompiler)
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function stripQuotes(string $value)
+    {
+        return Str::startsWith($value, ['"', '\''])
+        ? substr($value, 1, -1)
+        : $value;
+    }
+
 }
